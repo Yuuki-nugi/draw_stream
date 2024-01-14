8fca3ecfc248a6429100e6075e4aa434284fb353
Yuki-Kunugi <kunuyuu3@gmail.com>
2024-01-14 21:20:29 +0900

    Temporary implementation of sample app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,61 +1,105 @@
+import 'dart:io';
+
+import 'package:draw_stream/drawing_data.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:draw_stream/draw_stream.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+class MyApp extends HookWidget {
   final _drawStreamPlugin = DrawStream();
 
   @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _drawStreamPlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final outputVideoPath = useState<String?>(null);
+    final controller = useState<VideoPlayerController?>(null);
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 100),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: ElevatedButton(
+                  onPressed: () async {
+                    final ImagePicker picker = ImagePicker();
+                    // Pick an image.
+                    final XFile? video =
+                        await picker.pickVideo(source: ImageSource.gallery);
+                    if (video != null) {
+                      final videoFileName = video.name;
+
+                      Directory tmpDocDir = await getTemporaryDirectory();
+                      String tmpDocPath = tmpDocDir.path;
+                      String outputPath = '$tmpDocPath/$videoFileName.mp4';
+
+                      final drawingDataList = List<DrawingData>.filled(
+                          120,
+                          DrawingData(
+                            circles: [
+                              CircleData(
+                                  center: const Offset(100, 100),
+                                  radius: 20,
+                                  color: Colors.red,
+                                  strokeWidth: 20)
+                            ],
+                            lines: [
+                              LineData(
+                                  start: const Offset(100, 100),
+                                  end: const Offset(200, 200),
+                                  color: Colors.red,
+                                  strokeWidth: 20)
+                            ],
+                          ));
+                      try {
+                        await _drawStreamPlugin.startDrawStream(
+                          videoPath: video.path,
+                          outputPath: outputPath,
+                          drawingDataList: drawingDataList,
+                        );
+                        controller.value =
+                            VideoPlayerController.file(File(outputPath));
+
+                        controller.value!.setLooping(true);
+                        controller.value!.initialize();
+                        controller.value!.play();
+                      } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text(e.toString())));
+                      }
+                    }
+                  },
+                  child: Text('startDrawStream'),
+                ),
+              ),
+              if (controller.value != null)
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  child: Stack(
+                    alignment: Alignment.bottomCenter,
+                    children: <Widget>[
+                      VideoPlayer(controller.value!),
+                      VideoProgressIndicator(controller.value!,
+                          allowScrubbing: true),
+                    ],
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
     );
